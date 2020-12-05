275b301745199de01cdc893f4821e7c5fe805cdf
thamo2020 <thamodya01@gmail.com>
2020-12-05 23:59:03 +0530

    customize auth

--- blogging/resources/views/CustomAuth/newLogin.blade.php
+++ blogging/resources/views/CustomAuth/newLogin.blade.php
@@ -27,19 +27,31 @@ Blogging-Loging
                             <div class="text-center">
                                 <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
                             </div>
-                            <form class="user">
+                            <form method="POST" class="user" action="{{ route('login') }}">
+                        @csrf
+
                                 <div class="form-group">
-                                    <input type="email" class="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
+                                    <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
                                         placeholder="Enter Email Address...">
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>     
+                                        @enderror
                                 </div>
                                 <div class="form-group">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password">
+                                <input id="password" type="password" class="form-control form-controller-user @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" 
+                                         placeholder="Password">
+
+                                         @error('password')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
                                 </div>
                                 <div class="form-group">
                                     <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
+                                        <input class="form-check-input custom-control-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                         <label class="custom-control-label" for="customCheck">Remember
                                             Me</label>
                                     </div>
@@ -47,20 +59,14 @@ Blogging-Loging
                                 <a href="index.html" class="btn btn-primary btn-user btn-block">
                                     Login
                                 </a>
-                                <hr>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a>
+                               
                             </form>
                             <hr>
                             <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                <a class="small" href="{{ route('password.request') }}">Forgot Password?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
+                                <a class="small" href="{{route('register')}}">Create an Account!</a>
                             </div>
                         </div>
                     </div>
--- blogging/resources/views/CustomAuth/newRegister.blade.php
+++ blogging/resources/views/CustomAuth/newRegister.blade.php
@@ -10,9 +10,6 @@ Blogging-Register
 
 @section('content')
 
-<div class="container">
-
-<!-- Outer Row -->
 <div class="row justify-content-center">
 
     <div class="col-xl-6 col-lg-6 col-md-6">
@@ -21,46 +18,66 @@ Blogging-Register
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
-        <!--div class="col-lg-6 d-none d-lg-block bg-login-image"></div-->
+                    <!--div class="col-lg-5 d-none d-lg-block bg-register-image"></div-->
                     <div class="col-lg-12">
                         <div class="p-5">
                             <div class="text-center">
-                                <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                             </div>
-                            <form class="user">
-                                <div class="form-group">
-                                    <input type="email" class="form-control form-control-user"
-                                        id="exampleInputEmail" aria-describedby="emailHelp"
-                                        placeholder="Enter Email Address...">
-                                </div>
-                                <div class="form-group">
-                                    <input type="password" class="form-control form-control-user"
-                                        id="exampleInputPassword" placeholder="Password">
-                                </div>
-                                <div class="form-group">
-                                    <div class="custom-control custom-checkbox small">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        <label class="custom-control-label" for="customCheck">Remember
-                                            Me</label>
+                            <form method="POST" class="user" action="{{ route('register') }}">
+                        @csrf
+                                <div class="form-group row">
+                                    <div class="col-sm-12 mb-6 mb-sm-3">
+                                        <input id="name" type="text" class="form-control form-control-user @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                                            placeholder="Name">
+
+                                            @error('name')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
                                     </div>
+                                   
                                 </div>
-                                <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                    Login
+                                <div class="form-group">
+                                    <input id="email" type="email" class="form-control form-control-user @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email"
+                                        placeholder="Email Address">
+
+                                        @error('email')
+                                            <span class="invalid-feedback" role="alert">
+                                                <strong>{{ $message }}</strong>
+                                            </span>
+                                        @enderror
+                                </div>
+                                <div class="form-group row">
+                                    <div class="col-sm-6 mb-3 mb-sm-0">
+                                        <input id="password" type="password" class="form-control form-control-user @error('password') is-invalid @enderror" name="password" required autocomplete="new-password"
+                                            placeholder="Password">
+
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+
+                                    </div>
+                                    <div class="col-sm-6">
+                                        <input id="password-confirm" type="password" class="form-control form-control-user" name="password_confirmation" required autocomplete="new-password" placeholder="Repeat Password">
+                                    </div>
+
+
+                                </div>
+                                <a href="login.html" class="btn btn-primary btn-user btn-block">
+                                    Register Account
                                 </a>
-                                <hr>
-                                <a href="index.html" class="btn btn-google btn-user btn-block">
-                                    <i class="fab fa-google fa-fw"></i> Login with Google
-                                </a>
-                                <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                    <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                </a>
+                                
                             </form>
                             <hr>
                             <div class="text-center">
-                                <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                <a class="small" href="{{ route('password.request') }}">Forgot Password?</a>
                             </div>
                             <div class="text-center">
-                                <a class="small" href="register.html">Create an Account!</a>
+                                <a class="small" href="{{route('login')}}">Already have an account? Login!</a>
                             </div>
                         </div>
                     </div>
@@ -69,10 +86,6 @@ Blogging-Register
         </div>
 
     </div>
-
-</div>
-
-</div>
 
 
 @endsection
